8a90a59286294e96552402b1a9a9899f722a0684
Ederin (Ediri) <eigharoro@gmail.com>
2022-11-04 06:13:54 -0600

    feat(static): include static folder and add file path in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,45 @@
+
 module.exports = {
   siteMetadata: {
     title: `Ed-Matic`,
     siteUrl: `https://www.yourdomain.tld`,
     description: `Digital creation site for Ed-Matic`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-emotion",
+    {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "assets",
+        path: "./static/assets/",
+      },
+      __key: "assets",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+    }
+  ]
 };
