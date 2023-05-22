714755f2915d0331b409a2f3e40be3a45b44b8de
Ederin Igharoro <eigharoro@iteamnm.com>
2023-05-22 14:34:00 -0600

    @E-roro feat(home); change TopSection of the home page

--- src/components/pages/Home/TopSection.tsx
+++ src/components/pages/Home/TopSection.tsx
@@ -1,69 +1,55 @@
 import {
-  SectionGrid,
   SectionFlex,
   SectionContainer,
   SectionP
 } from "components/molecules/body"
+import {Interpolation, Theme } from '@emotion/react'
 import ReactPlayer from "react-player"
 import { mq } from "Theme/MediaQueries"
 
-export const TopSection = () => (
-  <SectionGrid
-    css={{
-      gridRow: 1,
-      margin: "auto",
-      alignContent: "space-evenly",
-      alignItems: "center",
-      position: "relative",
-      width: "100vw",
-      [mq.lg]: {
-        maxWidth: 1080,
-      },
-    }}
-  >
-    {/* Banner*/}
-    <SectionFlex
-      css={{
-        alignContent: "flex-end",
-        alignItems: "center",
-        flexDirection: "column",
-        justifyContent: "space-around",
-        justifyItems: "center",
-        flexWrap: "nowrap",
-        overflowWrap: "break-word",
-        [mq.lg]: {
-          gridColumn: "2/ span 1",
-          maxWidth: 1080,
-        },
-      }}
-    >
-      <ReactPlayer
-        css={{
-          padding:"100%"
-        } }
-        url='https://www.youtube.com/watch?v=cCA6MMjAib4'
-        speed={1.5}
-        playing={true}
-        muted={true}
-        loop={true}
-        controls={false}
-      />
+const MainGrid: Interpolation<Theme> = {
+  gridRow: 1,
+  margin: "auto",
+  alignContent: "space-evenly",
+  alignItems: "center",
+  position: "relative",
+  width: "100vw",
+  [mq.lg]: {
+    maxWidth: 1080,
+  },
+};
 
-      <SectionContainer>
-        <h1
-          css={{
-            fontFamily: "Poppins, Helvetica, Arial, Lucida, sans-serif",
-            fontSize: "32px",
-            overflowWrap: "break-word",
-            color: "white"
-          }}
-        >
-          Ed-Matic
-        </h1>
-        <SectionP css={{ lineHeight: "1.8em" }}>
-          Software Engineer | Indie Developer | Digital Creator
-        </SectionP>
-      </SectionContainer>
-    </SectionFlex>
-  </SectionGrid >
+
+export const TopSection = () => (
+  <SectionFlex css={MainGrid}>
+    {/* Banner*/}
+    <ReactPlayer
+      url='https://www.youtube.com/watch?v=cCA6MMjAib4'
+      speed={1.5}
+      playing={true}
+      muted={true}
+      loop={true}
+      controls={false}
+      style={{
+        width: "100vw",
+        height: "100vh",
+
+      }}
+    />
+    <SectionContainer>
+      <h1
+        css={{
+          fontFamily: "Poppins, Helvetica, Arial, Lucida, sans-serif",
+          fontSize: "32px",
+          overflowWrap: "break-word",
+          color: "white"
+        }}
+      >
+        Ed-Matic
+      </h1>
+      <SectionP css={{ lineHeight: "1.8em" }}>
+        Software Engineer | Indie Developer | Digital Creator
+      </SectionP>
+    </SectionContainer>
+  </SectionFlex >
 )
