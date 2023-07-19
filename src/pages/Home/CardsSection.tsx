de0dbbc55a64e3c329f0a6ad2460cf4dec3548bc
Ed-Matic <eigharoro@gmail.com>
2023-07-18 18:18:11 -0600

    refactor(cards): reorganize card section of home

--- src/pages/Home/CardsSection.tsx
+++ src/pages/Home/CardsSection.tsx
@@ -1,129 +1,84 @@
-import { useTheme } from "@emotion/react"
+import { Interpolation,} from "@emotion/react"
 import {
   SectionContainer,
-  SectionFlex,
   SectionGrid,
 } from "components/molecules/body"
 import { mq } from "Theme/MediaQueries"
+import Theme from "Theme/Theme"
+
+const MainGrid: Interpolation<Theme> = {
+  background: "#1E1C1C",
+  margin: "auto",
+  position: "relative",
+  width: "100%",
+  [mq.lg]: {
+    maxWidth: 1080,
+  },
+  gridRow: 3,
+}
+
+const BoxContainer: Interpolation<Theme> = {
+  background: "#3E3746",
+  padding: 50,
+  boxSizing: "border-box",
+}
 
 export const CardsSection = () => {
-  const theme = useTheme()
   return (
-    <SectionGrid
-      css={{
-        background: "#1E1C1C",
-        margin: "auto",
-        position: "relative",
-        width: "100%",
-        [mq.lg]: {
-          maxWidth: 1080,
-        },
-        gridRow: 3,
-      }}
-      display={"grid"}
-    >
-      <SectionFlex
-        css={{
-          height: "100%",
-          flexDirection: "column",
-          justifyContent: "center",
-          flexWrap: "nowrap",
-          margin: "auto",
-          position: "relative",
-          width: "100%",
-          padding: "50px 0",
-          [mq.lg]: {
-            flexDirection: "row",
-          },
-        }}
-      >
-        <SectionContainer
-          css={{
-            background: "#3E3746",
-            padding: 50,
-            boxSizing: "border-box",
-          }}
-        >
-          <h4 css={{ color: "rgba(0,0,0,0.4)!important" }}>Games</h4>
+    <SectionGrid css={MainGrid} display={"grid"}>
 
-          <h3>Game Development</h3>
+      <SectionContainer css={BoxContainer}>
+        <h4 css={{ color: "rgba(0,0,0,0.4)!important" }}>Games</h4>
 
-          <p css={{ color: "white" }}>
-            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
-            Vivamus magna justo, lacinia eget consectetur sed, convallis at
-            tellus.
-          </p>
-          <a href="" data-icon="="> View Details {"=>"} </a>
-        </SectionContainer>
-        <SectionContainer
-          css={{
-            padding: 50,
-            backgroundColor: "#182845",
-            boxSizing: "border-box",
-          }}
-        >
-          <h4 css={{ color: "rgba(255,255,255,0.4)!important" }}>Stories</h4>
-          <h3 css={{ color: "white" }}>Film & Animations</h3>
-          <p css={{ color: "white" }}>
-            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
-            Vivamus magna justo, lacinia eget consectetur sed, convallis at
-            tellus.
-          </p>
-          <a
-            href=""
-            data-icon="="
-          >
-            View Details
-          </a>
-        </SectionContainer>
-        <SectionContainer
-          css={{
-            background: theme.colors.primary,
-            //background: "white",
-            padding: 50,
+        <h3>Game Development</h3>
 
-            backgroundColor: "#004fe8",
-            boxSizing: "border-box",
-          }}
-        >
-          <h4 css={{ color: "rgba(255,255,255,0.4)!important" }}>Music</h4>
-          <h3 css={{ color: "white" }}>Music Production</h3>
-          <p css={{ color: "white" }}>
-            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
-            Vivamus magna justo, lacinia eget consectetur sed, convallis at
-            tellus.
-          </p>
-          <a
-            href=""
-            data-icon="="
-          >
-            View Details
-          </a>
-        </SectionContainer>
-        <SectionContainer
-          css={{
-            padding: 50,
-            background: "#3E3746",
+        <p css={{ color: "white" }}>
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+          Vivamus magna justo, lacinia eget consectetur sed, convallis at
+          tellus.
+        </p>
+        <a href="" data-icon="="> View Details {"=>"} </a>
+      </SectionContainer>
 
-            //background: "teal",
-            // boxSizing: "border-box",
-          }}
-        >
-          <h4 css={{ color: "rgba(0,0,0,0.4)!important" }}>PC Builds</h4>
-          <h3>Gaming & Workstations</h3>
-          <p css={{ color: "white" }}>
-            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
-            Vivamus magna justo, lacinia eget consectetur sed, convallis at
-            tellus.
-          </p>
-          <a
-            href=""
-            data-icon="="
-          >
-            View Details
-          </a>
-        </SectionContainer>
-      </SectionFlex>
+      <SectionContainer css={BoxContainer}>
+        <h4 css={{ color: "rgba(255,255,255,0.4)!important" }}>Stories</h4>
+
+        <h3 css={{ color: "white" }}>Film & Animations</h3>
+
+        <p css={{ color: "white" }}>
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+          Vivamus magna justo, lacinia eget consectetur sed, convallis at
+          tellus.
+        </p>
+        <a href="" data-icon="="> View Details </a>
+      </SectionContainer>
+
+      <SectionContainer css={BoxContainer}>
+        <h4 css={{ color: "rgba(255,255,255,0.4)!important" }}>Music</h4>
+
+        <h3 css={{ color: "white" }}>Music Production</h3>
+
+        <p css={{ color: "white" }}>
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+          Vivamus magna justo, lacinia eget consectetur sed, convallis at
+          tellus.
+        </p>
+        <a href="" data-icon="="> View Details </a>
+      </SectionContainer>
+
+      <SectionContainer css={BoxContainer} >
+        <h4 css={{ color: "rgba(0,0,0,0.4)!important" }}>PC Builds</h4>
+
+        <h3>Gaming & Workstations</h3>
+        
+        <p css={{ color: "white" }}>
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+          Vivamus magna justo, lacinia eget consectetur sed, convallis at
+          tellus.
+        </p>
+        <a href="" data-icon="="> View Details</a>
+      </SectionContainer>
+
     </SectionGrid>
   )
 }
