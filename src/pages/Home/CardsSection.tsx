5ee28f69d95acaeb6d9eab3e62c9b2b20726d267
Ederin Igharoro <eigharoro@iteamnm.com>
2024-02-13 17:20:05 -0700

    feat(cards): add logic to enable card select responsiveness

--- src/pages/Home/CardsSection.tsx
+++ src/pages/Home/CardsSection.tsx
@@ -3,24 +3,50 @@ import {
   SectionGrid,
 } from "components/molecules/body"
 import {CardSectionCSS} from '../../styles/Home'
+import {useState} from "react";
+
+// To-do : Create function to handle card zoom responsiveness
+const cardData = 
+    [
+      {
+        categoryType: "Games",
+        title: "Game Development",
+        description: "Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.\n" +
+            "        Vivamus magna justo, lacinia eget consectetur sed, convallis at\n" +
+            "        tellus."
+      },
+    ];
+
 export const CardsSection = () => {
+  
+  const [selectedCard, setSelectedCard] = useState(null);
+  
+  const handleCardClick = (index) => {
+    setSelectedCard(index === selectedCard ? null : index);
+  };
+  
   return (
     <SectionGrid css={CardSectionCSS.CardSectionGrid} display={"grid"}>
+      {[0, 1, 2, 3].map((index) => (
+        <SectionContainer key={index} css={`
+        ${CardSectionCSS.BoxContainer}
+        ${selectedCard === index ? 'enlarged' : ''}
+      `} className={'Card'}>
+      <h4 css={{ color: "rgba(0,0,0,0.4)!important" }}>Games</h4>
 
-      <SectionContainer css={CardSectionCSS.BoxContainer}>
-        <h4 css={{ color: "rgba(0,0,0,0.4)!important" }}>Games</h4>
+      <h3>Game Development</h3>
 
-        <h3>Game Development</h3>
+      <p css={{ color: "white" }}>
+        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+        Vivamus magna justo, lacinia eget consectetur sed, convallis at
+        tellus.
+      </p>
+      <a href="" data-icon="="> View Details {"=>"} </a>
+    </SectionContainer>
+      ))}
+      
 
-        <p css={{ color: "white" }}>
-          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
-          Vivamus magna justo, lacinia eget consectetur sed, convallis at
-          tellus.
-        </p>
-        <a href="" data-icon="="> View Details {"=>"} </a>
-      </SectionContainer>
-
-      <SectionContainer css={CardSectionCSS.BoxContainer}>
+      <SectionContainer css={CardSectionCSS.BoxContainer} className={'Card'}>
         <h4 css={{ color: "rgba(255,255,255,0.4)!important" }}>Stories</h4>
 
         <h3 css={{ color: "white" }}>Film & Animations</h3>
@@ -33,7 +59,7 @@ export const CardsSection = () => {
         <a href="" data-icon="="> View Details </a>
       </SectionContainer>
 
-      <SectionContainer css={CardSectionCSS.BoxContainer}>
+      <SectionContainer css={CardSectionCSS.BoxContainer} className={'Card'}>
         <h4 css={{ color: "rgba(255,255,255,0.4)!important" }}>Music</h4>
 
         <h3 css={{ color: "white" }}>Music Production</h3>
@@ -46,7 +72,7 @@ export const CardsSection = () => {
         <a href="" data-icon="="> View Details </a>
       </SectionContainer>
 
-      <SectionContainer css={CardSectionCSS.BoxContainer} >
+      <SectionContainer css={CardSectionCSS.BoxContainer} className={'Card'} >
         <h4 css={{ color: "rgba(0,0,0,0.4)!important" }}>PC Builds</h4>
 
         <h3>Gaming & Workstations</h3>
